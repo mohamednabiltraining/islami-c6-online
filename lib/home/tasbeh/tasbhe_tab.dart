20180960b2389fc52ae9222481d48a599bdefd1b
salma <shimaa.shams@gmail.com>
2022-08-01 04:55:29 +0200

    add seb7a feature

--- lib/home/tasbeh/tasbhe_tab.dart
+++ lib/home/tasbeh/tasbhe_tab.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
 
-class TasbehTab extends StatelessWidget {
+class TasbehTab extends StatefulWidget {
+  @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
+
+class _TasbehTabState extends State<TasbehTab> {
+  String mydoaa = 'سبحان الله';
+  int numberOfTasbe7 = 0;
+  double angle = 0.2;
+  int i = 0;
+
   @override
   Widget build(BuildContext context) {
+    int i = 0;
+
     return Container(
-      color: Colors.brown,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/background_pattern.png',
+          ),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                  alignment: Alignment.bottomRight,
+                  width: 70,
+                  height: 70,
+                  child:
+                      Image(image: AssetImage('assets/images/head_seb7a.png'))),
+              Container(
+                  width: 150,
+                  height: 150,
+                  child: Transform.rotate(
+                      angle: angle,
+                      child: Image(
+                          image: AssetImage('assets/images/body_seb7a.png')))),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Text('عدد التسبيحات',
+                      style: Theme.of(context).textTheme.headline4)),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(24)),
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 35),
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Text('${numberOfTasbe7}',
+                      style: Theme.of(context).textTheme.headline2)),
+              ElevatedButton(
+                  onPressed: () {
+                    onPress();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      primary: Theme.of(context).primaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24)) // background
+                      ),
+                  child: Text(mydoaa,
+                      style: Theme.of(context).textTheme.headline3))
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void onPress() {
+    List<String> doaa = ['سبحان الله', 'الحمد لله', 'الله أكبر'];
+    numberOfTasbe7++;
+    angle = angle + 0.1;
+    setState(() {});
+
+    if (numberOfTasbe7 == 33) {
+      numberOfTasbe7 = 0;
+      i++;
+    }
+    if (i == 1) {
+      mydoaa = doaa[1];
+      setState(() {});
+    } else if (i == 2) {
+      mydoaa = doaa[2];
+      setState(() {});
+    } else if (i == 3) {
+      i = 0;
+      mydoaa = doaa[0];
+      setState(() {});
+    }
   }
 }
--- lib/my_theme.dart
+++ lib/my_theme.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 class MyTheme {
   static final Color primaryColor = Color(0xFFB7935F);
+  static final Color cardColor = Color(0xF6B29B5A);
   static final ThemeData lightTheme = ThemeData(
       primaryColor: primaryColor,
+      cardColor: cardColor,
       scaffoldBackgroundColor: Colors.transparent,
       textTheme: TextTheme(
           bodyText1: TextStyle(fontSize: 18, color: Colors.black),
+          headline2: TextStyle(
+              fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold),
+          headline3: TextStyle(
+              fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
           headline4: TextStyle(fontSize: 28, color: Colors.black),
           headline5: TextStyle(fontSize: 24, color: Colors.black)),
       appBarTheme: AppBarTheme(
